40e4902f66a74f2ed3cf52677b112550ec231216
Jordan Tha <102616668+jtdt3@users.noreply.github.com>
2025-05-28 21:49:28 -0700

    commented output in main

--- main.cpp
+++ main.cpp
@@ -22,3 +22,20 @@ int main() {
     cout << "\nFinal message received: " << message << "\n";
     return 0;
 }
+
+// Output:
+//Message sent: Hello, Network!
+
+// ==== Sending ====
+// [Application Layer] Sending: Hello, Network!
+// [Transport Layer] Sending: APP_HDR|Hello, Network!
+// [Network Layer] Sending: TRANS_HDR|APP_HDR|Hello, Network!
+// [Link Layer] Sending: NET_HDR|TRANS_HDR|APP_HDR|Hello, Network!
+
+// ==== Receiving ====
+// [Link Layer] Receiving: LINK_HDR|NET_HDR|TRANS_HDR|APP_HDR|Hello, Network!
+// [Network Layer] Receiving: NET_HDR|TRANS_HDR|APP_HDR|Hello, Network!
+// [Transport Layer] Receiving: TRANS_HDR|APP_HDR|Hello, Network!
+// [Application Layer] Receiving: APP_HDR|Hello, Network!
+
+// Final message received: Hello, Network!
